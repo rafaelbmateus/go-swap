0ba4ccb25d4d03a117488076475cce2d7674fde6
Rafael Mateus <rafaelmateus@hotmail.com.br>
2022-04-17 23:06:26 -0300

    feat: add transfer files

---
+++ binance/transfer.go
@@ -0,0 +1,6 @@
+package binance
+
+// TransferToBinance transfer of binance to kucoin.
+func (me *Binance) TransferToKucoin() error {
+	return nil
+}
---
+++ kucoin/transfer.go
@@ -0,0 +1,6 @@
+package kucoin
+
+// TransferToBinance transfer of kucoin to binance.
+func (me *Kucoin) TransferToBinance() error {
+	return nil
+}
